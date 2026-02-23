92b406dfa17a6500fa388417793013efc63683f2
rick.huang <yenju0425@gmail.com>
2026-02-23 18:55:01 +0800

    fix:

--- src/112_PathSum/solution.h
+++ src/112_PathSum/solution.h
@@ -19,29 +19,4 @@ public:
 
 }  // namespace baseline
 
-namespace optimized {
-
-class Solution {
-private:
-    bool dfs(TreeNode* root, int remain) {
-        if (!root) {
-            return remain == 0;
-        }
-
-        remain -= root->val;
-        return dfs(root->left, remain) || dfs(root->right, remain);
-    }
-
-public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if (!root) {
-            return false;
-        }
-
-        return dfs(root, targetSum);
-    }
-};
-
-}  // namespace optimized
-
 }  // anonymous namespace
